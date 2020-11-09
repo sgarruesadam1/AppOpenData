eb4ea4490220412a93d8a72866acdde34e099080
eibarrolasdam1 <eibarrolasdam1@gmail.com>
2020-11-09 17:51:08 +0100

    Pantalla 2 "acabada"

--- opendata/lib/Screens/listaTipos_Screen.dart
+++ opendata/lib/Screens/listaTipos_Screen.dart
@@ -1,28 +1,38 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 import 'package:trabajo_open_data/providers/fiestas_provider.dart';
+import 'package:trabajo_open_data/screens/listaLocalidades_Screen.dart';
 
 class ListaTiposScreen extends StatelessWidget {
   Map<String, Object> args = new Map<String, Object>();
-  //final box = GetStorage();
+  final box = GetStorage();
 
   @override
   Widget build(BuildContext context) {
-    //box.write('mancomunidad', null);
-    //box.write('localidad', null);
-    //box.write('tipo', null);
+    box.write('nombreLocalidad', null);
+    //args = ModalRoute.of(context).settings.arguments;
+    args = Get.arguments ?? new Map<String, Object>();
     return Scaffold(
       appBar: AppBar(
-        title: Text("Tipos"),
+        title: Text("Tipos de Fiestas, Tradiciones y Eventos"),
       ),
       //drawer: MenuWidget(),
       body: _lista(context),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          //Navigator.pop(context);
+          Get.offAll(ListaLocalidadesScreen(), arguments: args);
+        },
+        child: Icon(Icons.arrow_back),
+      ),
     );
   }
 
   Widget _lista(BuildContext context) {
     return FutureBuilder(
-      future: fiestasProvider.cargarTipos(),
+      future: fiestasProvider
+          .cargarTipos(box.read('nombreLocalidad') ?? args['nombreLocalidad']),
       initialData: [],
       builder: (context, snapshot) {
         if (snapshot.connectionState == ConnectionState.done) {
@@ -42,12 +52,12 @@ class ListaTiposScreen extends StatelessWidget {
       final w = ListTile(
         title: Text(element),
         trailing: Icon(Icons.keyboard_arrow_right),
-        //onTap: () {
-        //box.write('mancomunidad', element);
-        //args['mancomunidad'] = element;
-        // Navigator.pushNamed(context, 'localidades', arguments: args);
-        //Get.offAll(ListaTipos(), arguments: args);
-        //},
+        onTap: () {
+          box.write('tipo', element);
+          args['tipo'] = element;
+          //Navigator.pushNamed(context, 'tipos', arguments: args);
+          Get.offAll(ListaNombresScreen(), arguments: args);
+        },
       );
       lst.add(w);
       lst.add(Divider());
